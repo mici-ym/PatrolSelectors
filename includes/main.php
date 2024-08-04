b250ff6d2e41be0fc0aa127cc28a8eaaf1b3f3cf
mici-ym <mrdky1@hotmail.com>
2024-08-04 17:01:03 +0300

    no exist rc_tags in object

--- includes/main.php
+++ includes/main.php
@@ -7,22 +7,16 @@ namespace MediaWiki\Extension\PatrolSelectors;
 
 use MediaWiki\Hook\RecentChange_saveHook;
 use MediaWiki\MediaWikiServices;
-
+use Wikimedia\Parsoid\Ext\JSON\JSON;
 
 class main implements RecentChange_saveHook {
 
     public function onRecentChange_save( $recentChange ) {
         $config = MediaWikiServices::getInstance()->getMainConfig();
         $nameSpaces = $config->get('PatrolSelectorsNameSpaces');
-
+        
         if (in_array($recentChange->getAttribute('rc_namespace'), $nameSpaces) && $recentChange->getAttribute('rc_patrolled') == 0) {
             $recentChange->reallyMarkPatrolled();
         }
-
-        $tage = $config->get('PatrolSelectorsTags');
-        if (in_array($recentChange->getAttribute('rc_tag'), $tage) && $recentChange->getAttribute('rc_patrolled') == 0) {
-            $recentChange->reallyMarkPatrolled();
-        }
-        
     }
 }
